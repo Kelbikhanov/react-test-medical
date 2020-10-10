913c8285aae492079083ccc9d33a76efdd17699a
kelbikhanov <rusik9091@gmail.com>
2020-10-10 07:31:19 +0300

    Updates

---
+++ precache-manifest.6f4a1ebc0b45d88c875e58c21b2fb4bd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de2e8612574ee03a59a71fa444ee30a",
+    "url": "/react-test-medical/index.html"
+  },
+  {
+    "revision": "323657191c03c38fdf8d",
+    "url": "/react-test-medical/static/css/2.8d3b5be9.chunk.css"
+  },
+  {
+    "revision": "4d2f895e04f92dd958eb",
+    "url": "/react-test-medical/static/css/main.5d7bcebf.chunk.css"
+  },
+  {
+    "revision": "323657191c03c38fdf8d",
+    "url": "/react-test-medical/static/js/2.c08d3774.chunk.js"
+  },
+  {
+    "revision": "ae96ce37367c3b15926eb6b17d3aa94e",
+    "url": "/react-test-medical/static/js/2.c08d3774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d2f895e04f92dd958eb",
+    "url": "/react-test-medical/static/js/main.769cacb2.chunk.js"
+  },
+  {
+    "revision": "c87ba7a135f3cd5c93cd",
+    "url": "/react-test-medical/static/js/runtime-main.01671f6a.js"
+  },
+  {
+    "revision": "95804ba38ea12bd3a70da90ef85cfe8d",
+    "url": "/react-test-medical/static/media/arrow.95804ba3.svg"
+  },
+  {
+    "revision": "fcc61eeaa225a3ddd555c5a806db05d3",
+    "url": "/react-test-medical/static/media/bell.fcc61eea.svg"
+  },
+  {
+    "revision": "36724cb25b4116b1c0662b96e25ade67",
+    "url": "/react-test-medical/static/media/eye.36724cb2.svg"
+  },
+  {
+    "revision": "0d5f1085931899693f7dc801a6c46e5e",
+    "url": "/react-test-medical/static/media/help.0d5f1085.svg"
+  },
+  {
+    "revision": "685d1d6b9b6656673f9fdcd2aa60705b",
+    "url": "/react-test-medical/static/media/open.685d1d6b.svg"
+  },
+  {
+    "revision": "f24e179e056af17a5bccbb57f61885b8",
+    "url": "/react-test-medical/static/media/search.f24e179e.svg"
+  }
+]);
